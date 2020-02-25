9f23250cccf596c18f7afaa6698aa70d992f9d78
*SunBo* <*799664881@qq.com*>
2020-02-25 15:49:50 +0800

    '完善数据库及添加数据,添加更新,删除数据'

--- app.py
+++ app.py
@@ -1,21 +1,127 @@
-from flask import Flask,render_template
+import os
+import sys
+import click
+
+from flask import Flask, render_template, request, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'GZzzq'
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'  # WIN 三个斜杠
+else:
+    prefix = 'sqlite:////'  # MAC Linux四个斜杠
+
+# 配置
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def index():
-    name = 'sunbo'
+# 创建数据库模型类
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+# 自定义指令 initdb
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='删除之后再创建')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('初始化数据库')
+
+
+# 自定义命令forge，把数据写入数据库
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = "GZzzq"
     movies = [
-        {'title': '扫赌1', 'year': '2003'},
-        {'title': '扫赌2', 'year': '2018'},
-        {'title': '扫赌3', 'year': '2016'},
-        {'title': '扫赌4', 'year': '2020'},
-        {'title': '扫赌5', 'year': '1989'},
-        {'title': '扫赌6', 'year': '2020'},
-        {'title': '扫赌7', 'year': '2020'},
-        {'title': '扫赌8', 'year': '2017'},
-        {'title': '扫赌9', 'year': '2005'},
-        {'title': '扫赌10', 'year': '2015'}
+        {'title': '杀破狼', 'year': '2003'},
+        {'title': '扫毒', 'year': '2018'},
+        {'title': '捉妖记', 'year': '2016'},
+        {'title': '囧妈', 'year': '2020'},
+        {'title': '葫芦娃', 'year': '1989'},
+        {'title': '玻璃盒子', 'year': '2020'},
+        {'title': '调酒师', 'year': '2020'},
+        {'title': '釜山行', 'year': '2017'},
+        {'title': '导火索', 'year': '2005'},
+        {'title': '叶问', 'year': '2015'},
     ]
-    return  render_template('index.html',name=name,movies=movies)
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo("数据导入完成")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # 获取表单数据
+        title = request.form.get('title')
+        year = request.form.get('year')
+
+        # 验证title year 不为空并且title 不大于60 year不大于4
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash("输入错误")
+            return redirect(url_for('index'))
+
+        movie = Movie(title=title, year=year)  # 创建记录
+        db.session.add(movie)  # 添加到数据库会话
+        db.session.commit()  # 提交数据库会话
+        flash("数据创建成功")
+        return redirect(url_for('index'))
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+
+
+# 编辑电影信息页面
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title=request.form['title']
+        year=request.form['year']
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash("输入错误")
+            return redirect(url_for('edit'),movie_id=movie_id)
+        movie.title=title
+        movie.year=year
+        db.session.commit()
+        flash('电影信息已经更新')
+        return redirect(url_for('index'))
+    return render_template('edit.html',movie=movie)
+# 删除电影信息页面
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('删除数据成功')
+    return redirect(url_for('index'))
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.context_processor  # 模板上下文处理函数
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
