974bed0f6a4d68151a77a0bb2905e09f818be611
*SunBo* <*799664881@qq.com*>
2020-02-29 19:36:57 +0800

    '将电影改为博客完成其功能增加富文本编辑器，未完成Bootstrap美化页面'

--- sunbo/commands.py
+++ sunbo/commands.py
@@ -1,6 +1,7 @@
 import click
+import  time
 from sunbo import app,db
-from sunbo.models import User,Movie
+from sunbo.models import User,Ariticles
 # 自定义initdb
 @app.cli.command()
 @click.option('--drop',is_flag=True,help='删除之后再创建')
@@ -14,24 +15,29 @@ def initdb(drop):
 @app.cli.command()
 def forge():
     db.create_all()
-    name = "SunBo"
-    movies = [
-        {'title':'杀破狼','year':'2003'},
-        {'title':'扫毒','year':'2018'},
-        {'title':'捉妖记','year':'2016'},
-        {'title':'囧妈','year':'2020'},
-        {'title':'葫芦娃','year':'1989'},
-        {'title':'玻璃盒子','year':'2020'},
-        {'title':'调酒师','year':'2020'},
-        {'title':'釜山行','year':'2017'},
-        {'title':'导火索','year':'2005'},
-        {'title':'叶问','year':'2015'}
+    author = "SunBo"
+    ariticles = [
+        {'title': '战疫小赋', 'content': '鼠年春节将至，突遭新冠疫情。明日山花烂漫，你我笑谈人生。'},
+        {'title': '向热爱致敬！', 'content': '在NBA效力了22年、已经43岁的卡特在场上依然会为了一次球权飞身救球，全力以赴就是自己对初心最好的交代！'},
+        {'title': '学习强国', 'content': '习近平总书记关切事｜确保不误农时，保障夏粮丰收——各地抓好春耕生产在行动'},
+        {'title': '你认为这个判罚是否合理？', 'content': '#孙杨遭禁赛8年# 你认为这个判罚是否合理？ R新浪体育的微博投票 ????'},
+        {'title': '疫情期间，你的工资有变化吗？',
+         'content': '疫情突如其来，许多企业的生产经营，遇到了程度不同的困难。近期，财政部出台调整部分企业所得税、免征部分行业增值税等政策减轻企业负担，央行也通过专项贷款等措施推动企业复工复产'},
+        {'title': '【塞斯-库里37+2+2实录】',
+         'content': '今日独行侠客场落败，独行侠球员塞斯-库里表现出色，首发出战36分钟，15投13中，其中三分球9投8中，得到37分2篮板2助攻，其中37分是小库里生涯得分新高，8记三分也创造了单场命中三分纪录！姓库里的都很准！'},
+        {'title': '乔治单臂暴扣', 'content': '伦纳德长臂一伸成功抢断快船发动反击，保罗-乔治接球冲筐单臂暴扣'},
+        {'title': '罗斯31分集锦',
+         'content': '今夜玫瑰绽放[鲜花]活塞客场挑战太阳，德里克-罗斯在30分钟的出场时间里，24投15中砍下31分4助攻3篮板2抢断1盖帽的全面数据。末节太阳疯狂追分，是罗斯连得4分帮助活塞捍卫胜果，射落太阳，终结7连败。来回顾罗斯今日超强表现！'},
+        {'title': '音乐知识', 'content': '扎西尼玛现场深情演唱一首《向往神鹰》，特有的豪迈之情，太赞了'},
+        {'title': '范巴斯滕', 'content': '范巴斯滕：说C罗比梅西强 要么不懂球要么在撒谎'}
     ]
-    user = User(name=name)
+    user = User(author=author)
+    localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
     db.session.add(user)
-    for m in movies:
-        movie = Movie(title=m['title'],year=m['year'])
-        db.session.add(movie)
+    for m in ariticles:
+        ariticles = Ariticles(title=m['title'],content=m['content'],pubdate=localtime)
+        db.session.add(ariticles)
     db.session.commit()
     click.echo('数据导入完成')
 
@@ -48,7 +54,7 @@ def admin(username,password):
         user.set_password(password)
     else:
         click.echo('创建用户')
-        user = User(username=username,name="雷洛")
+        user = User(userauthor=username,author="雷洛")
         user.set_password(password)
         db.session.add(user)
     
--- sunbo/views.py
+++ sunbo/views.py
@@ -1,85 +1,98 @@
-from sunbo import app,db
-from flask import request,redirect,url_for,flash,render_template
-from flask_login import login_user,logout_user,login_required,current_user
-from sunbo.models import User,Movie
+import time
+from sunbo import app, db
+from flask import request, redirect, url_for, flash, render_template
+from flask_login import login_user, logout_user, login_required, current_user
+from sunbo.models import User, Ariticles
+
+
 # 首页
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         if not current_user.is_authenticated:
             return redirect(url_for('index'))
         # 获取表单的数据
         title = request.form.get('title')
-        year = request.form.get('year')
+        content = request.form.get('content')
+        localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-        # 验证title，year不为空，并且title长度不大于60，year的长度不大于4
-        if not title or not year or len(year)>4 or len(title)>60:
+        if not title or not content or len(title) > 60:
             flash('输入错误')  # 错误提示
             return redirect(url_for('index'))  # 重定向回主页
-        
-        movie = Movie(title=title,year=year)  # 创建记录
-        db.session.add(movie)  # 添加到数据库会话
-        db.session.commit()   # 提交数据库会话
+
+        ariticles = Ariticles(title=title, content=content, pubdate=localtime)  # 创建记录
+        db.session.add(ariticles)  # 添加到数据库会话
+        db.session.commit()  # 提交数据库会话
         flash('数据创建成功')
         return redirect(url_for('index'))
 
-    movies = Movie.query.all()
-    return render_template('index.html',movies=movies)
-# 编辑电影信息页面
-@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+    ariticles = Ariticles.query.all()
+    return render_template('index.html', ariticles=ariticles)
+
+
+@app.route('/ariticles/detail/<int:ariticles_id>', methods=['GET', 'POST'])
 @login_required
-def edit(movie_id):
-    movie = Movie.query.get_or_404(movie_id)
+def detail(ariticles_id):
+    ariticles = Ariticles.query.get_or_404(ariticles_id)
+    return render_template('detail.html', ariticles=ariticles)
+
+
+# 编辑电影信息页面
+@app.route('/ariticles/edit/<int:ariticles_id>', methods=['GET', 'POST'])
+@login_required
+def edit(ariticles_id):
+    ariticles = Ariticles.query.get_or_404(ariticles_id)
 
     if request.method == 'POST':
         title = request.form['title']
-        year = request.form['year']
-
-        if not title or not year or len(year)>4 or len(title)>60:
+        content = request.form.get('content')
+        if not title or not content or len(title) > 60:
             flash('输入错误')
-            return redirect(url_for('edit'),movie_id=movie_id)
-        
-        movie.title = title
-        movie.year = year
+            return redirect(url_for('edit'), ariticles_id=ariticles_id)
+
+        ariticles.title = title
+        ariticles.content = content
         db.session.commit()
         flash('电影信息已经更新')
         return redirect(url_for('index'))
-    return render_template('edit.html',movie=movie)
+    return render_template('edit.html', ariticles=ariticles)
 
-@app.route('/settings',methods=['GET','POST'])
+
+@app.route('/settings', methods=['GET', 'POST'])
 @login_required
 def settings():
     if request.method == 'POST':
-        name = request.form['name']
+        author = request.form['author']
 
-        if not name or len(name)>20:
+        if not author or len(author) > 20:
             flash('输入错误')
             return redirect(url_for('settings'))
-        
-        current_user.name = name
+
+        current_user.name = author
         db.session.commit()
         flash('设置name成功')
         return redirect(url_for('index'))
 
     return render_template('settings.html')
 
+
 # 删除信息
-@app.route('/movie/delete/<int:movie_id>',methods=['POST'])
-@login_required    
-def delete(movie_id):
-    movie = Movie.query.get_or_404(movie_id)
-    db.session.delete(movie)
+@app.route('/ariticles/delete/<int:ariticles_id>', methods=['POST'])
+@login_required
+def delete(ariticles_id):
+    ariticles = Ariticles.query.get_or_404(ariticles_id)
+    db.session.delete(ariticles)
     db.session.commit()
     flash('删除数据成功')
     return redirect(url_for('index'))
 
+
 # 用户登录 flask提供的login_user()函数
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
         if not username or not password:
             flash('输入错误')
             return redirect(url_for('login'))
@@ -92,6 +105,7 @@ def login():
         return redirect(url_for('login'))
     return render_template('login.html')
 
+
 # 用户登出
 @app.route('/logout')
 @login_required
@@ -100,6 +114,5 @@ def logout():
     flash('退出登录')
     return redirect(url_for('index'))
 
-
-#pip install python-dotenv
-#现在，当您执行run命令时，应自动设置env var。
+# pip install python-dotenv
+# 现在，当您执行run命令时，应自动设置env var。
